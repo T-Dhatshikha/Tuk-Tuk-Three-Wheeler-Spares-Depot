7028e7a6e55b83085b6ac0ab72b8e3efd08532a7
Dhatshikha Thiruchelvam <dhatshikha.20250211@iit.ac.lk>
2026-07-21 22:34:56 +0530

    Add AuditLogger with append only file writing

---
+++ src/AuditLogger.java
@@ -0,0 +1,32 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class AuditLogger {
+    private String logFilePath = "data/audit_log.txt";
+
+    // METHOD 1 - Write one log entry
+    public void log(String action, String itemCode, int quantity) {
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(logFilePath, true));
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String timestamp = now.format(format);
+            String logLine = timestamp + " , " + action + " , " + itemCode + " , Quantity: " + quantity;
+            writer.write(logLine);
+            writer.newLine();
+            writer.close();
+            System.out.println("LOG: " + logLine);
+        } catch (IOException error) {
+            System.out.println("Error in writing to audit log: " + error.getMessage());
+        }
+    }
+
+    // METHOD 2 - Log without quantity (for delete)
+    public void log(String action, String itemCode) {
+        log(action, itemCode, 0);
+    }
+}
